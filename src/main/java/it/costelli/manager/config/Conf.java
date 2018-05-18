815a9f0ba45de297e36d069580bc191f79ded301
joker1618 <federicobarbano@gmail.com>
2018-05-18 02:27:23 +0200

    row 4

--- src/main/java/it/costelli/manager/config/Conf.java
+++ src/main/java/it/costelli/manager/config/Conf.java
@@ -8,9 +8,9 @@ import java.nio.file.Paths;
  */
 public class Conf {
 
-	public static final Path BASE_FOLDER = Paths.get("C:\\Users\\f.barbano\\IdeaProjects\\app\\CostelliManager\\src\\main\\resources");
+	public static final Path BASE_FOLDER = Paths.get("src/main/resources");
 
-	public static final Path RESOURCE_FIELDS_POS = BASE_FOLDER.resolve("pdf\\fieldsPositions.csv");
-	public static final Path RESOURCE_TEMPLATE_TEST_SHEET = BASE_FOLDER.resolve("pdf\\testSheetTemplate.pdf");
+	public static final Path RESOURCE_FIELDS_POS = BASE_FOLDER.resolve("pdf/fieldsPositions.csv");
+	public static final Path RESOURCE_TEMPLATE_TEST_SHEET = BASE_FOLDER.resolve("pdf/testSheetTemplate.pdf");
 
 }
