e2db4001a7f648bef054733910db960660ebae1e
joker1618 <federicobarbano@gmail.com>
2018-05-19 02:41:02 +0200

    ok editor

--- src/main/java/it/costelli/manager/launcher/FieldPosEditorGUI.java
+++ src/main/java/it/costelli/manager/launcher/FieldPosEditorGUI.java
@@ -6,6 +6,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.scenicview.ScenicView;
@@ -33,7 +36,12 @@ public class FieldPosEditorGUI extends Application {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/fieldPosEditorView.fxml"));
 		VBox editorPosPane = fxmlLoader.load();
 
-		Scene scene = new Scene(editorPosPane);
+		ScrollPane imagePane = createImagePane();
+
+		BorderPane borderPane = new BorderPane(imagePane, null, editorPosPane, null, null);
+
+		Scene scene = new Scene(borderPane);
+//		Scene scene = new Scene(editorPosPane);
 		stage.setScene(scene);
 		stage.show();
 
@@ -47,6 +55,21 @@ public class FieldPosEditorGUI extends Application {
 	@Override
 	public void stop() throws Exception {
 		logger.info("end");
+	}
+
+	private ScrollPane createImagePane() {
+		Image image = new Image(getClass().getResourceAsStream("/images/TestSheetTemplate.png"));
+		ImageView imageView = new ImageView(image);
+
+		double rel = image.getHeight() / image.getWidth();
+		double fitWidth = 1000.0;
+		imageView.setFitWidth(fitWidth);
+		imageView.setFitHeight(fitWidth * rel);
+
+		ScrollPane scrollPane = new ScrollPane(imageView);
+		scrollPane.setPrefWidth(fitWidth + 16.0);
+
+		return scrollPane;
 	}
 
 }
